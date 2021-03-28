ef13bb81c2c3a00c9b53bc32ab0a1d185bc0b0c2
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-03-29 01:45:31 +0600

    ema-john client code

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCUopuhUwuQ6P5W1g0eXhsX72HNL6RRLSI",
+  authDomain: "ema-john-6ae1c.firebaseapp.com",
+  projectId: "ema-john-6ae1c",
+  storageBucket: "ema-john-6ae1c.appspot.com",
+  messagingSenderId: "623977907383",
+  appId: "1:623977907383:web:155ae59077a289623af932"
+};
+export default firebaseConfig;
